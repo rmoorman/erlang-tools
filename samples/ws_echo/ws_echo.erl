827068e921cca94e92bc95066504feaaf17ddbf1
Dominique Boucher <schemeway@gmail.com>
2011-06-03 12:51:50 -0400

    Updated the example.

--- samples/ws_echo/ws_echo.erl
+++ samples/ws_echo/ws_echo.erl
@@ -4,7 +4,7 @@
 -module(ws_echo).
 
 %% websocket_mod behaviour functions
--export([init/1, shutdown/1, handle_data/2, handle_info/2]).
+-export([init/1, terminate/1, handle_data/2, handle_info/2]).
 
 -behaviour(websocket_mod).
 
@@ -12,7 +12,7 @@ init(_) ->
     error_logger:info_msg("Initializing echo handler~n"),
     [].
 
-shutdown(_State) ->
+terminate(_State) ->
     error_logger:info_msg("Stopping echo handler~n"),
     ok.
 
@@ -22,5 +22,5 @@ handle_info(_, _State) ->
     ignore.
 
 handle_data(Data, State) ->
-    websocket_mod:send_message(self(), {echo, Data}),
+    websocket_mod:cast({echo, Data}),
     State.
